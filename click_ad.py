445f04184d443fa5944235adba689381fffea93b
borenwu007 <borenwu10@163.com>
2019-12-11 18:35:18 +0800

    2019-12-11 18:33

--- click_ad.py
+++ click_ad.py
@@ -3,16 +3,23 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 import zipfile
 import time 
-# from get_random_link import get_link
+from get_random_link import get_link
 
+file_name = 'links.txt'
 class_name = 'ad_link'
- 
-driver = webdriver.Chrome()
-driver.get("http://taotuman.com/2019/12/10/blog-entry-1395.html")
-time.sleep(1)
 
-driver.switch_to.frame(0)
-elem = WebDriverWait(driver,100).until(lambda dirver:driver.find_element_by_class_name(class_name))
-elem.click()
-time.sleep(2)
-driver.quit()
+while(1):
+    try:
+        link = get_link(file_name)
+        driver = webdriver.Chrome()
+        driver.get(link)
+        time.sleep(1)
+
+        driver.switch_to.frame(0)
+        elem = WebDriverWait(driver,100).until(lambda dirver:driver.find_element_by_class_name(class_name))
+        elem.click()
+        time.sleep(5)
+        driver.quit()
+    except:
+        print("未知异常")
+        driver.quit()
---
+++ get_random_link.py
@@ -0,0 +1,10 @@
+import random
+
+def get_link(file):
+    f = open(file)
+    lines = f.readlines()
+    filessize = len(lines)
+    index = random.randint(0,filessize)
+    link = lines[index]
+    f.close()
+    return link
